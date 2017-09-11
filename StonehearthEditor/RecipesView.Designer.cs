c427255926377faba3e067cceb71df4ac6d4317e
Linda <lcai@riotgames.com>
2017-09-11 15:10:19 -0700

    Don't allow adding and removing of rows directly from grid view

--- StonehearthEditor/RecipesView.Designer.cs
+++ StonehearthEditor/RecipesView.Designer.cs
@@ -38,6 +38,8 @@
             // 
             // recipesGridView
             // 
+            this.recipesGridView.AllowUserToAddRows = false;
+            this.recipesGridView.AllowUserToDeleteRows = false;
             this.recipesGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
