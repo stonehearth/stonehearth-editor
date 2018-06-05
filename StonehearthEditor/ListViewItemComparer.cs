60690d624958459a1119b0b9812c8a097f9af48b
Luke Rinard <lrinard@riotgames.com>
2018-06-05 14:59:12 -0700

    Sort "none" as zero in numerical columns

--- StonehearthEditor/ListViewItemComparer.cs
+++ StonehearthEditor/ListViewItemComparer.cs
@@ -33,6 +33,19 @@ namespace StonehearthEditor
             int i1, i2;
             bool r1 = int.TryParse(s1, out i1);
             bool r2 = int.TryParse(s2, out i2);
+
+            //convert "none" to 0 when appropriate (when one side is a number and the other is "none")
+            if (r1 && !r2 && s2 == "none")
+            {
+                i2 = 0;
+                r2 = true;
+            }
+            else if (r2 && !r1 && s1 == "none")
+            {
+                i1 = 0;
+                r1 = true;
+            }
+            
             returnVal = r1 && r2 ? i1.CompareTo(i2) : string.Compare(s1, s2);
             if (order == SortOrder.Descending)
                 returnVal *= -1;
